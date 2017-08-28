373f7e1d431f4f7140210d4659aecf52738d9001
Augusto Azevedo & Filmon <pair+aazevedojr.filmonfeme@devbootcamp.com>
2017-08-28 16:41:48 -0700

    Implement user registration.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'questions/index'
+  get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
 
   root to:"questions#index"
   resources :questions
+  resources :users, except: [:create, :new]
 end
