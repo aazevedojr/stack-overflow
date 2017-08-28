fbfb7d26cc2149492a22583ab332c4054e13607b
Augusto Azevedo & Filmon <pair+aazevedojr.filmonfeme@devbootcamp.com>
2017-08-28 14:26:08 -0700

    Add associations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :questions, foreign_key: :questioner_id
+  has_many :answers, foreign_key: :answerer_id
+  has_many :comments, foreign_key: :commenter_id
+  has_many :votes, foreign_key: :voter_id
 end
