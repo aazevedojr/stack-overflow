e33c9a2cb5c2b9e08bdcf985562d863699dcc949
Augusto Azevedo & Filmon <pair+aazevedojr.filmonfeme@devbootcamp.com>
2017-08-28 14:18:53 -0700

    Generate migrations and models

---
+++ db/migrate/20170828211620_create_votes.rb
@@ -0,0 +1,12 @@
+class CreateVotes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :votes do |t|
+      t.integer :value
+      t.string :votable_type
+      t.integer :votable_id
+      t.integer :voter_id
+
+      t.timestamps
+    end
+  end
+end
