d0f9e207f06a78e8bc16c5c9db28fa02fec37f31
Augusto Azevedo & Filmon <pair+aazevedojr.filmonfeme@devbootcamp.com>
2017-08-28 17:28:24 -0700

    Implement user login/logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ApplicationRecord
   validates :email, format: {with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/}
 
   has_secure_password
+
+  # Returns the hash digest of the given string.
+  # (Advanced Login)
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
 
-  root to:"questions#index"
+  root to: 'questions#index'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :questions
   resources :users, except: [:create, :new]
 end
